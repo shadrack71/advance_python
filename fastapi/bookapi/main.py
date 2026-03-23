8b79b4d01215758533799325c05d6b5a9db0e0a2
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-03-24 00:23:14 +0300

    Add book management endpoints to FastAPI application

--- fastapi/bookapi/main.py
+++ fastapi/bookapi/main.py
@@ -91,6 +91,7 @@ async  def get_header(accept:str = Header(None),
 
     return request_header
 
+#  Creating Simple CRUD
 
 class GetBook(BaseModel):
     id:int
@@ -101,6 +102,12 @@ class GetBook(BaseModel):
     page_count:int
     language:str
 
+class BookUpdateModel(BaseModel):
+    title: str
+    author: str
+    publisher: str
+    page_count: int
+    language: str
 @app.get('/get_books',response_model=List[GetBook])
 async  def get_all_book()->list:
     return  books
@@ -119,11 +126,23 @@ async  def get_all_book(book_id:int):
             return  book
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail='Not found')
 
-
 @app.patch('/update_books/{book_id}')
-async  def update_all_book(book_id)->dict:
-    pass
+async  def update_all_book(book_id:int , book_update_data:BookUpdateModel)->dict:
+    for book in books:
+        if book['id'] == book_id:
+            book['title'] = book_update_data.title
+            book['publisher'] = book_update_data.publisher
+            book['page_count'] = book_update_data.page_count
+            book['language'] = book_update_data.language
 
-@app.delete('/delete_books')
-async  def delete_all_book(book_id)->dict:
-    pass
+            return book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+@app.delete('/delete_books/{book_id}',status_code=status.HTTP_204_NO_CONTENT)
+async  def delete_all_book(book_id:int):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return  {}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
