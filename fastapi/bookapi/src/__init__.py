fa5c5f0edb7aee38c6367bfb676d1076c58e1744
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-03-24 06:34:15 +0300

    Add database configuration and initialize async database connection

--- fastapi/bookapi/src/__init__.py
+++ fastapi/bookapi/src/__init__.py
@@ -2,11 +2,27 @@ from fastapi import FastAPI
 
 from .books.routes import book_router
 
+from .db.main import initdb
+
+from contextlib import asynccontextmanager
+
+@asynccontextmanager
+async def life_span(app:FastAPI):
+    print(f" server is starting")
+    await initdb()
+
+    yield
+
+    print(f" server is stoping")
+
+
+
 version = 'v1'
 app = FastAPI(
     title='bookly',
     description='A RESTful API for a book review web service',
-    version=version
+    version=version,
+    lifespan=life_span
 )
 
 app.include_router(book_router,prefix=f"/api/{version}/books",tags=['books'])
---
+++ fastapi/bookapi/src/books/models.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+
+class Book(SQLModel , table=True):
+    __tablename__ = "book"
+
+    uid:uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False
+        )
+    )
+
+    title: str
+    author: str
+    publisher: str
+    published_date: str
+    page_count: int
+    language:str
+    created_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+    updated_at:datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+    def __repr__(self) -> str:
+        return f"<Book {self.title}>"
