b98db85c8630e583f1ef6a6e32210eb8a4bb26e7
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-03-25 05:43:03 +0300

    Implement CRUD operations for books in BookService

--- fastapi/bookapi/src/books/service.py
+++ fastapi/bookapi/src/books/service.py
@@ -1,17 +1,48 @@
 from sqlmodel.ext.asyncio.session import AsyncSession
-
+from schemas import BookCreateModel,BookUpdateModel
+from sqlmodel import select, desc
+from models import  Book
 
 class BookService:
 
     async def get_all_books(self,session:AsyncSession):
-        pass
-
-
+        statement = select(Book).order_by(desc(Book.created_at))
+        result = await session.exec(statement)
+        return  result.all()
 
     async def get_book(self,book_uid:str,session:AsyncSession):
-        pass
+        statement = select(Book).where(Book.uid ==book_uid)
+        result = await session.exec(statement)
+        book = result.first()
+        return book if book is not None else None
 
+    async def create_books(self,book_data:BookCreateModel,session:AsyncSession):
+        book_data_dict = book_data.model_dump()
+        new_book = Book(
+            **book_data_dict
+        )
+        session.add(new_book)
+        await session.commit()
+        return new_book
 
+    async def update_books(self,book_uid:str,update_data:BookUpdateModel,session:AsyncSession):
+        book_to_update = self.get_book(book_uid,session)
+        if book_to_update is not None:
+            update_book = update_data.model_dump()
+            for k ,v in update_book.items():
+                setattr(book_to_update,k,v)
+            await session.commit()
 
-    async def create_books(self,book_data,session:AsyncSession):
-        pass
+            return  book_to_update
+        else:
+            return  None
+
+    async def delete_books(self,book_uid:str,session:AsyncSession):
+
+        book_to_delete = self.get_book(book_uid,session)
+        if book_to_delete is not None:
+            await session.delete(book_to_delete)
+            await session.commit()
+            return True
+        else:
+            return None
