fdac4e7053055439ed0e0357336e4a71061ca55f
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-03-28 03:38:32 +0300

    Add JWT authentication support with user login functionality and password hashing

--- fastapi/bookapi/src/auth/routes.py
+++ fastapi/bookapi/src/auth/routes.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from fastapi import  APIRouter ,Depends,status,HTTPException
+from fastapi.responses import JSONResponse
 
 from ..db.main import get_session
 from .schemas import UserCreateModel, UserModel, UserLoginModel
@@ -8,6 +11,8 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 
 auth_router = APIRouter()
 user_service = UserService()
+
+REFRESH_TOKEN_EXPIRY = 2
 
 @auth_router.post('/signup', response_model=UserModel,status_code=status.HTTP_201_CREATED)
 async def  create_user_account(user_data:UserCreateModel,session:AsyncSession = Depends(get_session)):
@@ -26,6 +31,30 @@ async def user_login(user_login_data:UserLoginModel,session:AsyncSession = Depen
     password = user_login_data.password
     user_exist = await user_service.user_exist(email,session)
     if user_exist:
-        user_data = user_service.get_user_email(email,session)
-        password_match = password_verify(user_data['password_hash'],password)
+        user_data = await user_service.get_user_email(email,session)
+        password_match = password_verify(password,user_data.password_hash)
+        if password_match:
+            user_payload = {
+                'email':user_data.email,
+                'user_uid':str(user_data.uid)
+            }
+            refresh_token = create_access_token(user_payload,refresh=True, expiry=timedelta(days=REFRESH_TOKEN_EXPIRY))
+            access_token = create_access_token(user_payload)
+
+            return  JSONResponse(
+                content={
+                    "message":"login Successful",
+                    "access_token":access_token,
+                    "refresh_token":refresh_token,
+                    "user":{
+                        "email":user_data.email,
+                        "uid":str(user_data.uid)
+                    }
+                }
+            )
+        else:
+            return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='Invalid password and email')
+    else:
+        return HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail='user not found')
+
     pass
