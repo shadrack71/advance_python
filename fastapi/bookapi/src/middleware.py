6369b743396c8d3aa4d196cc9c3210aaaafa2d28
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-04-04 22:46:43 +0300

    Add CORS and TrustedHost middleware for enhanced security and request handling

--- fastapi/bookapi/src/middleware.py
+++ fastapi/bookapi/src/middleware.py
@@ -1,6 +1,9 @@
 from  fastapi import  FastAPI ,status
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
+
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 import time
 import logging
 
@@ -34,5 +37,17 @@ def register_middleware(app:FastAPI):
         response = await  call_next(request)
 
         return response
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+        allow_credentials=True,
+    )
+
+    app.add_middleware(
+        TrustedHostMiddleware,
+        allowed_hosts=["localhost", "127.0.0.1"],
+    )
 
 
