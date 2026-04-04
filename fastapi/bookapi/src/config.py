22855abcc2a5f6d8713b4fc75311b5d308970413
shadrackkinsimba <shadrackkinsimba@gmail.com>
2026-04-04 23:10:19 +0300

    Add email configuration and setup for sending emails

--- fastapi/bookapi/src/config.py
+++ fastapi/bookapi/src/config.py
@@ -8,6 +8,18 @@ class Settings(BaseSettings):
     JWT_ALGORITHM:str
     REDIS_HOST:str
     REDIS_PORT:int = 6379
+    # email setting
+    MAIL_USERNAME: str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
+    MAIL_FROM_NAME: str
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
+    USE_CREDENTIALS: bool = True
+    VALIDATE_CERTS: bool = True
+    DOMAIN: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
---
+++ fastapi/bookapi/src/mail.py
@@ -0,0 +1,24 @@
+from fastapi_mail import FastMail , ConnectionConfig
+from .config import Config
+from pathlib import Path
+
+
+# get the parent directory
+BASE_DIR = Path(__file__).resolve().parent
+# create the config for sending emails
+mail_config = ConnectionConfig(
+    MAIL_USERNAME=Config.MAIL_USERNAME,
+    MAIL_PASSWORD=Config.MAIL_PASSWORD,
+    MAIL_FROM=Config.MAIL_FROM,
+    MAIL_PORT=587,
+    MAIL_SERVER=Config.MAIL_SERVER,
+    MAIL_FROM_NAME=Config.MAIL_FROM_NAME,
+    MAIL_STARTTLS=True,
+    MAIL_SSL_TLS=False,
+    USE_CREDENTIALS=True,
+    VALIDATE_CERTS=True,
+    TEMPLATE_FOLDER=Path(BASE_DIR, "templates"),
+)
+
+
+# create the object to send emails with the config
